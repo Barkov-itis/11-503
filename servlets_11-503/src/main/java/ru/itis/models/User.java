f9efe9b1e0c208abbd350d1375ac6caeb8989fe6
danil-Itis <tron5268@mail.ru>
2026-09-23 18:02:12 +0300

    23.09 mvn tomcat
    23/09 mvn tomcat

---
+++ servlets_11-503/src/main/java/ru/itis/models/User.java
@@ -0,0 +1,17 @@
+package ru.itis.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private Integer age;
+}
